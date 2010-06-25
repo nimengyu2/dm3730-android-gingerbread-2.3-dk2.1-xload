djwillis/x-loader-omap-pandora

Add support for beagle and overo
Signed-off-by: Steve Sakoman <steve@sakoman.com>

Makefile
@@ -147,8 +147,14 @@ omap3430labrador_config :    unconfig
 	@./mkconfig $(@:_config=) arm omap3 omap3430labrador
 
 omap3evm_config :	unconfig
-
 	@./mkconfig $(@:_config=) arm omap3 omap3evm
+
+overo_config :	unconfig
+	@./mkconfig $(@:_config=) arm omap3 overo
+
+omap3530beagle_config :    unconfig
+
+	@./mkconfig $(@:_config=) arm omap3 omap3530beagle
 
 #########################################################################
 

nimengyu2/dm3730-android-gingerbread-2.3-dk2.1-xload

Signed-off-by: Vaibhav Hiremath <hvaibhav@ti.com>

Makefile
@@ -152,6 +152,12 @@ omap3evm_config :	unconfig
 am3517evm_config :       unconfig
 	@./mkconfig $(@:_config=) arm omap3 am3517evm
 
+overo_config :	unconfig
+	@./mkconfig $(@:_config=) arm omap3 overo
+
+omap3beagle_config :    unconfig
+	@./mkconfig $(@:_config=) arm omap3 omap3beagle
+
 #########################################################################
 
 clean: