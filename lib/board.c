7b684e294d5a031d00a4769a6cf73350ac220b6d
Vaibhav Hiremath <hvaibhav@ti.com>
2011-04-08 19:02:52 +0530

    Update Version number to 1.51
    To track features -
    	- 34xx & 36xx DPLL settings for Beagle

--- lib/board.c
+++ lib/board.c
@@ -40,7 +40,7 @@ extern u32 get_mem_type(void);
 #ifdef CFG_PRINTF
 int print_info(void)
 {
-	printf("\n\nTexas Instruments X-Loader 1.50 ("
+	printf("\n\nTexas Instruments X-Loader 1.51 ("
 			__DATE__ " - " __TIME__ ")\n");
 	return 0;
 }
